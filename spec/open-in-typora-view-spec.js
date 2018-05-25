cd3d19850c5f0c3eb0da6eac972625fac539a683
erikwithuhk <efjonsson@gmail.com>
2018-05-25 11:29:15 -0400

    initial commit

---
+++ spec/open-in-typora-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OpenInTyporaView from '../lib/open-in-typora-view';
+
+describe('OpenInTyporaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
