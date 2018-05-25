e9d7d1964a409d3fe6c7786676a040e8c244f7c0
erikwithuhk <efjonsson@gmail.com>
2018-05-25 19:09:31 -0400

    cleanup

--- spec/open-in-typora-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import OpenInTyporaView from '../lib/open-in-typora-view';
-
-describe('OpenInTyporaView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
