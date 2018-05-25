df3c5441e0658f283229f30e2fdb8cae37f1a917
erikwithuhk <efjonsson@gmail.com>
2018-05-25 13:01:52 -0400

    implement menu logic

---
+++ menus/loadMenus.js
@@ -0,0 +1,57 @@
+'use babel';
+
+const regex = /\.md\b/;
+
+const isMarkdown = (filename) => {
+  const matches = regex.exec(filename);
+  return Array.isArray(matches) && matches.length > 0;
+};
+
+const shouldDisplay = (e) => {
+  const treeViewPath = e.target.dataset.path;
+  const textEditorDocumentTitle = e.view.document.title;
+  if (isMarkdown(treeViewPath) || isMarkdown(textEditorDocumentTitle)) {
+    return true;
+  }
+  return false;
+};
+
+const loadMenus = () => {
+  const contextMenu = atom.contextMenu.add({
+    'atom-text-editor': [
+      {
+        label: 'Open in Typora',
+        command: 'open-in-typora:open',
+        shouldDisplay,
+      },
+    ],
+    '.tree-view .file': [
+      {
+        label: 'Open in Typora',
+        command: 'open-in-typora:open',
+        shouldDisplay,
+      },
+    ],
+  });
+
+  const menu = atom.menu.add([
+    {
+      label: 'Packages',
+      submenu: [
+        {
+          label: 'Open in Typora',
+          submenu: [
+            {
+              label: 'Open in Typora',
+              command: 'open-in-typora:open',
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return { contextMenu, menu };
+};
+
+export default loadMenus;
